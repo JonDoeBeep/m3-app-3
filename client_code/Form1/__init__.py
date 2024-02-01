809eaf04de0108bd1cbc7b7aa56ac2845baead8b
keatonmcmichael@gmail.com <keatonmcmichael@gmail.com>
2024-02-01 15:25:13 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,16 +9,27 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
+      """This method is called when the button is clicked"""
+      self.text_box_1.text = self.button_1.text
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    self.text_box_1.text = self.button_2.text
 
   def button_3_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    self.text_box_1.text = self.button_3.text
+
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.text_box_1.text = self.button_4.text
+
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.text_box_1.text = self.button_4.text
+
+
+
 
   
     
