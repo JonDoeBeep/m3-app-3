f72a1082a7d0fcdc752029de98061e8ca23c6653
michaelbearbeanshot@gmail.com <michaelbearbeanshot@gmail.com>
2024-02-01 15:32:42 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,7 +11,8 @@ class Form1(Form1Template):
 
   def button_1_click(self, **event_args):
       """This method is called when the button is clicked"""
-      self.text_box_1.text = self.button_1.text
+      self.num = self.num + 
+      self.text_box_1.text = self.num
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
