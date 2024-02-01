c60a187ee5d3c55616a2c67322f03aaeae87ca94
keatonmcmichael@gmail.com <keatonmcmichael@gmail.com>
2024-02-01 15:28:56 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,7 +7,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-  def 
+
 
   def equate_click(self, **event_args):
     """This method is called when the button is clicked"""
