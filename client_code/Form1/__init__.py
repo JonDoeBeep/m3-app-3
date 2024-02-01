1f60f7568dfe0f09f0076933c3fa40c1e7d93e5a
michaelbearbeanshot@gmail.com <michaelbearbeanshot@gmail.com>
2024-02-01 15:31:32 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,12 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import numpy as np
 
 class Form1(Form1Template):
+  num = ''
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-  def 
+  
